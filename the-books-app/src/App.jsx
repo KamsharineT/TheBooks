6f7cac2f39db310b4494914293e713d1b3f4c9d8
KamsharineT <kamsharine.t@hotmail.com>
2025-10-07 23:16:37 +0800

    Update App.jsx and index.css

--- the-books-app/src/App.jsx
+++ the-books-app/src/App.jsx
@@ -1,35 +1,5 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-
-const Card = ({title}) =>{
-  // Manage component state (data that changes)
-  // const [variableName,setVariableName] = useState() --- Common practice
-  // state is persistent accross the browser, if you reload the component will be returned to the initial value
-  const [count,setCount] = useState(0)
-  const [hasLiked,setHasLiked] = useState(false)
-
-  useEffect(() => {
-    console.log(`${title} has been liked :${hasLiked}`)
-  }, [hasLiked]);
-
-  return(
-    <div className='card' onClick={() => setCount((prevState) => prevState + 1)}>
-      <h2>{title} <br/> {count} </h2>
-      <button onClick={() => {setHasLiked(!hasLiked)}}>
-        {hasLiked ? '❤️' : 'Like🤍'}
-      </button>
-    </div>
-  )
+const App = () => {
+  return (<h1 class="text-3xl font-bold underline">    Hello world!  </h1>);
 }
-const App = () =>{
-  
-  return (
-    <div className='card-container'>
-      <Card title={"Book Thief"}/>
-      <Card title={"Mid Night Library"}/>
-      <Card title={"Ikigai"}/>
-    </div>
-  )
-}
-
-export default App
+ 
+export default App;
